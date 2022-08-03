7a08c331102b624bdf69c41ea4f6b03dac24f7f2
Amar Cheema <amr.cheema@yahoo.com>
2022-08-03 19:49:53 +0100

    Added function to generate quote

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ console.log('hi');
 const sportsQuotes = ["'I don\'t count my situps. I only start counting once it starts hurting.' - Muhammad Ali", "'Cancer can take away all my physical abilities. It cannot touch my mind, it cannot touch my heart and it cannot touch my soul. And those three things are going to carry on forever.' - Jimmy Valvano", "'I\'ve failed over and over again in my life. And that is why I succeed' - Michael Jordan", "'The only way to prove you are a good sport is to lose.' - Ernie Banks", "'There may be people that have more talent than you, but there\'s no excuse for anyone to work harder than you do.' - Derek Jeter", "'If you fail to prepare, you\'re prepared to fail.' - Mark Spitz", "'The road to Easy Street goes through the sewer.' - John Madden", "'Stubbornness usually is considered a negative; but I think that trait has been a positive for me.' - Cal Ripken, Jr", "'Wisdom is always an overmatch for strength.' - Phil Jackson", "'Never say never because limits, like fears, are often just an illusion.' - Michael Jordan"];
 const randomNumber = Math.floor(Math.random() * sportsQuotes.length);
 
-console.log(randomNumber, sportsQuotes.length);
+const generateFunction = () => console.log(sportsQuotes[randomNumber]);
+
+generateFunction();
